f7b13dcda572e2bee2f817eb0edf93c21d6d12a2
Geo Thomas <geot@mit.edu>
2014-10-03 15:51:05 -0400

    Change build scripts

---
+++ ci/RunDlyScripts.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+env=$ENV
+if [ "$env" == "kcdev" -o "$env" == "all" ] 
+then
+	sudo -u www cp -u /var/lib/jenkins/jobs/kc_mit_6_0/workspace/kcmit-webapp/target/kcmit-webapp.6.0.0.1.war /usr/local/tomcat_kc_dev/webapps/kc-dev.war
+	sleep 1
+	#sudo -u www chown www:www /usr/local/tomcat_kc_dev/webapps/kc-dev.war
+	#sleep 1
+fi
+if [ "$env" == "kcqawkly" -o "$env" == "all" ]
+then
+	sudo -u www scp /var/lib/jenkins/jobs/kc_mit_6_0/workspace/kcmit-webapp/target/kcmit-webapp.6.0.0.1.war www@kc-qa-web:/usr/local/tomcat_kc_qa/webapps/kc-qa.war
+	sleep 1
+	sudo -u coeus cp -u /var/lib/jenkins/jobs/kc_mit_6_0/workspace/kcmit-webapp/target/kcmit-webapp.6.0.0.1.war /home/coeus/kc/deployment/.
+	sleep 1
+fi
