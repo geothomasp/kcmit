f6d9ece356553c0b67dc4192925217528ecc89c0
Geo Thomas <geot@mit.edu>
2014-10-05 22:45:35 -0400

    fixing qawkly deployment issue ;

--- ci/RunDlyScripts.sh
+++ ci/RunDlyScripts.sh
@@ -11,6 +11,6 @@ if [ "$env" == "kcqawkly" -o "$env" == "all" ]
 then
 	sudo -u www scp /var/lib/jenkins/jobs/kc_mit_6_0/workspace/kcmit-webapp/target/kcmit-webapp-6.0.0.1.war www@kc-qa-web:/usr/local/tomcat_kc_qa/webapps/kc-qa-wkly.war
 	sleep 1
-	sudo -u coeus cp -u /var/lib/jenkins/jobs/kc_mit_6_0/workspace/kcmit-webapp/target/kcmit-webapp.6.0.0.1.war /home/coeus/kc/deployment/.
+	sudo -u coeus cp -u /var/lib/jenkins/jobs/kc_mit_6_0/workspace/kcmit-webapp/target/kcmit-webapp-6.0.0.1.war /home/coeus/kc/deployment/.
 	sleep 1
 fi
