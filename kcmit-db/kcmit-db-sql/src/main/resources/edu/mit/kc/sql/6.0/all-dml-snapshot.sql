db9b88563001b18071223690e9a02390fa4ed0e9
vineeth <vineeth@OSP414.mit.edu>
2014-10-07 18:10:13 -0400

    MITKC-467-Negotiation custom dat issue fixed

--- kcmit-db/kcmit-db-sql/src/main/resources/edu/mit/kc/sql/6.0/all-dml-snapshot.sql
+++ kcmit-db/kcmit-db-sql/src/main/resources/edu/mit/kc/sql/6.0/all-dml-snapshot.sql
@@ -1,4 +1,5 @@
 @dml/upd_latest_doc_typ_id_09302014.sql
 @dml/DML_MITKC-465_10072014.sql
+@dml/DML_MITKC-467_10072014.sql
 commit
 /
---
+++ kcmit-db/kcmit-db-sql/src/main/resources/edu/mit/kc/sql/6.0/dml/DML_MITKC-467_10072014.sql
@@ -0,0 +1,2 @@
+UPDATE KCSO.CUSTOM_ATTRIBUTE SET LOOKUP_CLASS='org.kuali.coeus.common.framework.person.KcPerson' WHERE LOOKUP_CLASS='org.kuali.kra.bo.KcPerson'
+/
