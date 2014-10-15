0aa5c374ba764ac54494c1daa58204526a25d0ae
Geo Thomas <geot@mit.edu>
2014-10-14 23:33:00 -0400

    Adding shell script to run sql

---
+++ ci/RunSqlScripts.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+env=$ENV
+scriptDir="../kcmit-db/kcmit-db-sql/src/main/resources/edu/mit/kc/sql/6.0"
+tod=$(date +%Y%m%d)
+echo $tod
+echo "spool sqlrun-${tod}.log"
+cd $scriptDir
+sqlplus kcso/dev50kc@KCDEV @all-sql-snapshot.sql
+sqlplus kcso/qawkly50kc@KCQAWKLY @all-sql-snapshot.sql
+cat all-ddl-snapshot.sql > "all-ddl-$tod.sql"
+cat all-dml-snapshot.sql > "all-dml-$tod.sql"
+> all-ddl-snapshot.sql;
+> all-dml-snapshot.sql;
+git add $scriptDir/.
+git commit -am 'reset all snapshots'
+git push origin
