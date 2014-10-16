e25178dfed7f700d813e215f6a2abca8cd2eee12
Geo Thomas <geot@mit.edu>
2014-10-16 10:56:50 -0400

    reset all-migration-snapshot

--- kcmit-db/kcmit-db-sql/src/main/resources/edu/mit/kc/sql/migration/all-migration-snapshot.sql
+++ kcmit-db/kcmit-db-sql/src/main/resources/edu/mit/kc/sql/migration/all-migration-snapshot.sql
@@ -1,7 +1,2 @@
-@DML_MITKC-32_09172014.sql
-@document_access_eps_01092014.sql
-@document_access_irb_10102014.sql
-@insert_derived_role_patch_10152014.sql
-@delete_dup_document_access_10152014.sql
 EXIT
 /
