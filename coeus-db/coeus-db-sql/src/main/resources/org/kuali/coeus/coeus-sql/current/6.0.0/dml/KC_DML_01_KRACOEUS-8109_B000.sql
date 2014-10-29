8a388db442312d8e5cd4911ea8c900e5714e6a4e
Geo Thomas <geot@mit.edu>
2014-10-29 16:38:07 -0400

    changed sql syntax for KC_DML_01_KRACOEUS-8109_B000.sql

--- coeus-db/coeus-db-sql/src/main/resources/org/kuali/coeus/coeus-sql/current/6.0.0/dml/KC_DML_01_KRACOEUS-8109_B000.sql
+++ coeus-db/coeus-db-sql/src/main/resources/org/kuali/coeus/coeus-sql/current/6.0.0/dml/KC_DML_01_KRACOEUS-8109_B000.sql
@@ -1,4 +1,3 @@
-UPDATE KRMS_FUNC_T
-SET NM = 'completeNarrativeRule' AND DESC_TXT= 'Complete Narrative Rule'
-where FUNC_ID IN (select FUNC_ID from KRMS_FUNC_T where NM= 'incompleteNarrativeRule')
+UPDATE KRMS_FUNC_T SET NM = 'completeNarrativeRule',DESC_TXT= 'Complete Narrative Rule' 
+	where FUNC_ID IN (select FUNC_ID from KRMS_FUNC_T where NM= 'incompleteNarrativeRule')
 /
