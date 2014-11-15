ca7dcaa8896c7d58a19c2019d56f4e1225d68723
Geo Thomas <geot@mit.edu>
2014-11-15 05:50:37 -0500

    reset all snapshots

--- ci/RunSqlScripts.sh
+++ ci/RunSqlScripts.sh
@@ -3,10 +3,11 @@
 #export LD_LIBRARY_PATH=$ORACLE_HOME/lib
 #export PATH=$PATH:$LD_LIBRARY_PATH:$ORACLE_HOME/bin:/usr/local/bin
 #env=$ENV
-scriptDir="../kcmit-db/kcmit-db-sql/src/main/resources/edu/mit/kc/sql/6.0"
+scriptDir="../kcmit-db/kcmit-db-sql/src/main/resources/edu/mit/kc/sql"
 cd $scriptDir
-#git pull --rebase origin master
+git pull --rebase origin master
 tod=$(date +%Y%m%d%H%M)
+cd 6.0
 spool sqlrun-${tod}.log
 sqlplus kcso/dev50kc@KCDEV @all-sql-snapshot.sql
 sqlplus kcso/qawkly50kc@KCQAWKLY @all-sql-snapshot.sql
@@ -17,7 +18,7 @@ cat all-dml-snapshot.sql > "all-dml-$tod.sql"
 > all-ddl-snapshot.sql;
 > all-dml-snapshot.sql;
 cd ../migration
-echo "spool migrun-${tod}.log"
+spool migrun-${tod}.log
 #sqlplus kcso/dev50kc@KCDEV @all-migration-snapshot.sql
 #sqlplus kcso/qawkly50kc@KCQAWKLY @all-migration-snapshot.sql
 sqlplus kcso/train50kc@KCTRAIN @all-migration-snapshot.sql
@@ -25,6 +26,7 @@ spool off
 cat all-migration-snapshot.sql > "all-migration-$tod.sql"
 > all-migration-snapshot.sql;
 #git checkout master
-#git add .
-#git commit -am 'reset all snapshots'
-#git push origin master
+cd $scriptDir
+git add .
+git commit -am 'reset all snapshots'
+git push origin master
