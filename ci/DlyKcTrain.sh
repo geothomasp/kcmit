1da332bcd0a775f25129422cac81b832b0dae4a1
Geo Thomas <geot@mit.edu>
2014-11-17 00:53:32 -0500

    adding train deployment script

--- ci/DlyKcTrain.sh
+++ ci/DlyKcTrain.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo -u www cp -u /var/lib/jenkins/jobs/kc_mit_6_0/workspace/kcmit-webapp/target/kcmit-webapp-6.0.0.1.war /usr/local/tomcat-kc-train/webapps/kc-train.war
+sudo -u www cp -u /var/lib/jenkins/jobs/kc_mit_6_0/workspace/kcmit-webapp/target/kcmit-webapp-6.0.0.1.war www@kc-train-web:/usr/local/tomcat-kc-train/webapps/kc-train.war
