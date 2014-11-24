b5eedf1b64e5369d6acf5038e4c9b372dffb6c51
sasipolus <sasi.kumar@polussoftware.com>
2014-11-24 16:01:28 +0530

    KRACOEUS-8355 Parameters to identify the comment code types.

---
+++ coeus-db/coeus-db-sql/src/main/resources/org/kuali/coeus/coeus-sql/current/6.0.0/dml/KC_DML_01_KRACOEUS-8355_B000.sql
@@ -0,0 +1,21 @@
+Insert into KRCR_PARM_T (NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID) values
+('KC-GEN','All','PROPOSAL_IP_REVIEW_COMMENT_CODE',SYS_GUID(),1,'CONFG','16','Proposal IP review code to identify comment code type','A','KC');
+/
+Insert into KRCR_PARM_T (NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID) values
+('KC-GEN','All','PROPOSAL_SUMMARY_COMMENT_CODE',SYS_GUID(),1,'CONFG','12','Proposal summary code to identify comment code type','A','KC');
+/
+Insert into KRCR_PARM_T (NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID) values
+('KC-GEN','All','SPECIAL_RATE_COMMENT_CODE',SYS_GUID(),1,'CONFG','7','Special rate code to identify comment code type','A','KC');
+/
+Insert into KRCR_PARM_T (NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID) values
+('KC-GEN','All','SPECIAL_REVIEW_COMMENT_CODE',SYS_GUID(),1,'CONFG','10','Special review code to identify comment code type','A','KC');
+/
+Insert into KRCR_PARM_T (NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID) values
+('KC-GEN','All','FISCAL_REPORT_COMMENT_CODE',SYS_GUID(),1,'CONFG','3','Fiscal report code to identify comment code type','A','KC');
+/
+Insert into KRCR_PARM_T (NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID) values
+('KC-GEN','All','INDIRECT_COST_COMMENT_CODE',SYS_GUID(),1,'CONFG','8','Comment type code for Indirect cost Comments','A','KC');
+/
+Insert into KRCR_PARM_T (NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID) values
+('KC-GEN','All','INVOICE_INSTRUCTION_COMMENT_CODE',SYS_GUID(),1,'CONFG','1','Comment type code for Invoice instructions','A','KC');
+commit;
