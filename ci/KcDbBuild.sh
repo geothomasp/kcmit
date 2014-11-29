7873725d3911ceec25fbb9233e1574d7173b91e3
Geo Thomas <geot@mit.edu>
2014-11-28 23:35:52 -0500

    shell script fix

--- ci/KcDbBuild.sh
+++ ci/KcDbBuild.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-scriptDir="../kcmit-db/kcmit-db-sql/src/main/resources/edu/mit/kc/sql"
-script60Dir="../../kc/coeus-db/coeus-db-sql/src/main/resources/org/kuali/coeus/coeus-sql/current/6.0.0" 
+echo "Script executed from: ${PWD}"
+BASEDIR=$(dirname $0)
+scriptDir="$BASEDIR/../kcmit-db/kcmit-db-sql/src/main/resources/edu/mit/kc/sql"
+script60Dir="$BASEDIR/../../kc/coeus-db/coeus-db-sql/src/main/resources/org/kuali/coeus/coeus-sql/current/6.0.0" 
 
 if [ $# -lt 1 ]
 then
