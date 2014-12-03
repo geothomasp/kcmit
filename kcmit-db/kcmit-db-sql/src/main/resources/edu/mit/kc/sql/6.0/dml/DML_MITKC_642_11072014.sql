b228bf953288b72d4a9aef3e05aa7f175e04e5d0
anjali <anjali.k@polussoftware.com>
2014-12-03 19:17:38 +0530

    For disabling the protocol linking in proposal development,award and institutional proposal

---
+++ kcmit-db/kcmit-db-sql/src/main/resources/edu/mit/kc/sql/6.0/dml/DML_MITKC_642_11072014.sql
@@ -0,0 +1,13 @@
+update KRCR_PARM_T set VAL = 'Y' where PARM_NM= 'iacuc.protocol.institute.proposal.linking.enabled';
+/
+update KRCR_PARM_T set VAL = 'Y' where PARM_NM= 'irb.protocol.institute.proposal.linking.enabled';
+
+update KRCR_PARM_T set VAL = 'Y' where PARM_NM= 'iacuc.protocol.award.linking.enabled';
+/
+update KRCR_PARM_T set VAL = 'Y' where PARM_NM= 'irb.protocol.award.linking.enabled';
+/
+update KRCR_PARM_T set VAL = 'Y' where PARM_NM= 'iacuc.protocol.proposal.development.linking.enabled';
+/
+update KRCR_PARM_T set VAL = 'Y' where PARM_NM= 'irb.protocol.development.proposal.linking.enabled';
+/  
+commit
