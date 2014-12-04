bce6586ed5318d1cae2698347ea81c4d9a83eb92
anishk <anishk@polus148.polusindia.com>
2014-12-04 10:41:48 +0530

    MITKC-438

--- kcmit-db/kcmit-db-sql/src/main/resources/edu/mit/kc/sql/migration/Coeus_KCRoleRightsMigration/scripts/syncing_role_rights.sql
+++ kcmit-db/kcmit-db-sql/src/main/resources/edu/mit/kc/sql/migration/Coeus_KCRoleRightsMigration/scripts/syncing_role_rights.sql
@@ -346,3 +346,30 @@ end;
 /
 commit
 /
+--MITKC-438
+INSERT INTO KRIM_ROLE_PERM_T(ROLE_PERM_ID,OBJ_ID,VER_NBR,ROLE_ID,PERM_ID,ACTV_IND)
+VALUES(KRIM_ROLE_PERM_ID_S.nextval,sys_guid(),1,
+(select role_id from krim_role_t where  role_nm = 'OSP Administrator'),
+(select perm_id from krim_perm_t where nm = 'Blanket Approve RICE Document'),
+'Y')
+/
+INSERT INTO KRIM_ROLE_PERM_T(ROLE_PERM_ID,OBJ_ID,VER_NBR,ROLE_ID,PERM_ID,ACTV_IND)
+VALUES(KRIM_ROLE_PERM_ID_S.nextval,sys_guid(),1,
+(select role_id from krim_role_t where  role_nm = 'OSP Administrator'),
+(select perm_id from krim_perm_t where nm = 'Blanket Approve Document'),
+'Y')
+/
+INSERT INTO KRIM_ROLE_PERM_T(ROLE_PERM_ID,OBJ_ID,VER_NBR,ROLE_ID,PERM_ID,ACTV_IND)
+VALUES(KRIM_ROLE_PERM_ID_S.nextval,sys_guid(),1,
+(select role_id from krim_role_t where  role_nm = 'OSP Administrator'),
+(select perm_id from krim_perm_t where nm = 'Blanket Approve AwardDocument'),
+'Y')
+/
+INSERT INTO KRIM_ROLE_PERM_T(ROLE_PERM_ID,OBJ_ID,VER_NBR,ROLE_ID,PERM_ID,ACTV_IND)
+VALUES(KRIM_ROLE_PERM_ID_S.nextval,sys_guid(),1,
+(select role_id from krim_role_t where  role_nm = 'OSP Administrator'),
+(select perm_id from krim_perm_t where nm = 'Blanket Approve AwardBudgetDocument'),
+'Y')
+/
+commit
+/
