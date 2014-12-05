afb730168ca7e57855ae0bc39936dcee121e3fe2
Geo Thomas <geot@mit.edu>
2014-12-05 09:33:33 -0500

    fix the type of notification email id

--- kcmit-db/kcmit-db-sql/src/main/resources/edu/mit/kc/sql/notification/dml_notification.sql
+++ kcmit-db/kcmit-db-sql/src/main/resources/edu/mit/kc/sql/notification/dml_notification.sql
@@ -5,19 +5,21 @@ Insert into KRCR_PARM_T (NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VA
 values ('KC-GEN','All','KC_DB_MAIL_PORT',sys_guid(),1,'CONFG','25','A','Set mail port for notificaion sent from DB','KC')
 /
 Insert into KRCR_PARM_T (NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,EVAL_OPRTR_CD,PARM_DESC_TXT,APPL_ID)
-values ('KC-GEN','All','KC_DB_SENDER_ID',sys_guid(),1,'CONFG','kc-notification@mit.edu','A','Set sender id for DB notification','KC')
+values ('KC-GEN','All','KC_DB_SENDER_ID',sys_guid(),1,'CONFG','kc-notifications@mit.edu','A','Set sender id for DB notification','KC')
 /
 Insert into KRCR_PARM_T (NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,EVAL_OPRTR_CD,PARM_DESC_TXT,APPL_ID)
-values ('KC-GEN','All','KC_DB_REPLY_ID',sys_guid(),1,'CONFG','kc-notification@mit.edu','A','Set reply to id for DB notification','KC')
+values ('KC-GEN','All','KC_DB_REPLY_ID',sys_guid(),1,'CONFG','kc-notifications@mit.edu','A','Set reply to id for DB notification','KC')
 /
 -- new added start
-UPDATE KRCR_PARM_T SET VAL = 'kc-notification@mit.edu' WHERE PARM_NM = 'EMAIL_NOTIFICATION_FROM_ADDRESS' and NMSPC_CD='KC-GEN'
+UPDATE KRCR_PARM_T SET VAL = 'kc-notifications@mit.edu' WHERE PARM_NM = 'EMAIL_NOTIFICATION_FROM_ADDRESS' and NMSPC_CD='KC-GEN'
 /
-UPDATE KRCR_PARM_T SET VAL = 'kc-notification@mit.edu' WHERE PARM_NM = 'EMAIL_NOTIFICATION_TEST_ADDRESS' and NMSPC_CD='KC-GEN'
+UPDATE KRCR_PARM_T SET VAL = 'kc-notifications@mit.edu' WHERE PARM_NM = 'EMAIL_NOTIFICATION_TEST_ADDRESS' and NMSPC_CD='KC-GEN'
 /
-UPDATE KRCR_PARM_T SET VAL = 'kc-notification@mit.edu' WHERE PARM_NM = 'EMAIL_NOTIFICATION_TEST_ADDRESS' and NMSPC_CD='KR-WKFLW'
+UPDATE KRCR_PARM_T SET VAL = 'kc-notifications@mit.edu' WHERE PARM_NM = 'EMAIL_NOTIFICATION_TEST_ADDRESS' and NMSPC_CD='KR-WKFLW'
 /
-UPDATE KRCR_PARM_T SET VAL = 'kc-notification@mit.edu' WHERE PARM_NM = 'FROM_ADDRESS' and NMSPC_CD='KR-WKFLW'
+UPDATE KRCR_PARM_T SET VAL = 'kc-notifications@mit.edu' WHERE PARM_NM = 'FROM_ADDRESS' and NMSPC_CD='KR-WKFLW'
+/
+UPDATE KRCR_PARM_T SET VAL = 'kc-notifications@mit.edu' WHERE PARM_NM = 'LOOKUP_CONTACT_EMAIL' and NMSPC_CD='KC-GEN'
 /
 UPDATE KRCR_PARM_T SET VAL = 'Y' WHERE PARM_NM = 'EMAIL_NOTIFICATIONS_ENABLED' and NMSPC_CD='KC-GEN'
 /
