25c52c870707bd20078a301468ebb3876ff2fc87
anumole <anumole@polus177.polusindia.com>
2015-01-09 14:23:10 +0530

    KRACOEUS-8674 - Following issue that, User name for update changes to "kr" instead of logged in  user name has been addressed

--- coeus-impl/src/main/java/org/kuali/coeus/sys/framework/model/KcTransactionalDocumentBase.java
+++ coeus-impl/src/main/java/org/kuali/coeus/sys/framework/model/KcTransactionalDocumentBase.java
@@ -40,6 +40,7 @@ import org.kuali.rice.krad.exception.ValidationException;
 import org.kuali.rice.krad.rules.rule.event.DocumentEvent;
 import org.kuali.rice.krad.util.ErrorMessage;
 import org.kuali.rice.krad.util.GlobalVariables;
+import org.kuali.rice.krad.util.KRADConstants;
 import org.kuali.rice.krad.util.LegacyDataFramework;
 
 import javax.persistence.*;
@@ -182,7 +183,9 @@ public abstract class KcTransactionalDocumentBase extends TransactionalDocumentB
 
     @Override
     public void setUpdateUser(String updateUser) {
-        this.updateUser = updateUser;
+    	if (!KRADConstants.SYSTEM_USER.equals(updateUser)) {
+            this.updateUser = StringUtils.substring(updateUser, 0, UPDATE_USER_LENGTH);
+        }
     }
 
     @Override
