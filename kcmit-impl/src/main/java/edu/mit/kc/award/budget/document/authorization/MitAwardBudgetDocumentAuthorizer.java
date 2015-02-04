5bc15b47d198eda1c43a60ef2e497e1a76eba074
vineeth <vineeth@mit.edu>
2015-02-04 17:18:08 -0500

    MITKC-1039 - Removed Disapprove from work flow

---
+++ kcmit-impl/src/main/java/edu/mit/kc/award/budget/document/authorization/MitAwardBudgetDocumentAuthorizer.java
@@ -0,0 +1,14 @@
+package edu.mit.kc.award.budget.document.authorization;
+
+import org.kuali.kra.award.budget.document.authorization.AwardBudgetDocumentAuthorizer;
+import org.kuali.rice.kim.api.identity.Person;
+import org.kuali.rice.krad.document.Document;
+
+public class MitAwardBudgetDocumentAuthorizer extends AwardBudgetDocumentAuthorizer{
+	
+	  @Override
+	    public boolean canDisapprove( Document document, Person user ) {
+	        return false;
+	    }
+
+}
