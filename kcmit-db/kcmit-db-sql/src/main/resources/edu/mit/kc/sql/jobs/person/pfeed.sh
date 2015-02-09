63c053dbfe08a0ac83e876acfe3e19613186fb4e
Geo Thomas <geot@mit.edu>
2015-02-08 22:08:00 -0500

    file name changes in feed and jobs

---
+++ kcmit-db/kcmit-db-sql/src/main/resources/edu/mit/kc/sql/jobs/person/pfeed.sh
@@ -0,0 +1,17 @@
+echo `date` 'Starting pfeed.sh ***Kuali Coeus Dev Database***'
+
+export ORACLE_HOME=/oracle/product/11.2.0.4/db/
+export LD_LIBRARY_PATH=$ORACLE_HOME/lib
+export PATH=$PATH:$LD_LIBRARY_PATH:$ORACLE_HOME/bin:/usr/local/bin
+export TNS_ADMIN=/oracle/network/admin/
+scriptDir="/opt/kc/feeds/person/"
+cd $scriptDir
+tod=$(date +%Y%m%d%H%M)
+echo $tod
+echo "spool pfeed-${tod}.log"
+ORACLE_CONNECT_STRING=`cat /opt/kc/dbcreds/kcdeveid`
+sqlplus $ORACLE_CONNECT_STRING @person_feed.sql > logs/pfeed-${tod}.log
+
+echo `date` 'End pfeed.sh'
+
+mail -s 'Person Feed - Dev Database - KCSO' kc-mit-dev@mit.edu < /opt/kc/feeds/person/logs/pfeed-${tod}.log
