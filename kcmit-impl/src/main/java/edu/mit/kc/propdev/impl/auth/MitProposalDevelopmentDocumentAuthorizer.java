cd30b176a51123bfbbbaa5bb6a4edd344da850d5
vineeth <vineeth@mit.edu>
2015-02-12 18:36:28 -0500

    MITKC-1064-Disable disapprove button in proposal development

---
+++ kcmit-impl/src/main/java/edu/mit/kc/propdev/impl/auth/MitProposalDevelopmentDocumentAuthorizer.java
@@ -0,0 +1,14 @@
+package edu.mit.kc.propdev.impl.auth;
+
+import org.kuali.coeus.propdev.impl.auth.ProposalDevelopmentDocumentAuthorizer;
+import org.kuali.rice.kim.api.identity.Person;
+import org.kuali.rice.krad.document.Document;
+
+public class MitProposalDevelopmentDocumentAuthorizer extends ProposalDevelopmentDocumentAuthorizer{
+	
+	@Override
+	public boolean canDisapprove(Document document, Person user) {
+		return false;
+	}
+
+}
