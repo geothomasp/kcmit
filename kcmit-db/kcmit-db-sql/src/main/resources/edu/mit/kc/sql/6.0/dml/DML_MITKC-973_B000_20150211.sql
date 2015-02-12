6f38dd597ca9bd40ae747f58570c18084e8ce021
vineeth <vineeth@mit.edu>
2015-02-12 18:32:44 -0500

    MITKC-973- KRMS - Award Namespace - function "All Awards" and Derived role of Lead Unit OSP_ADMINISTRATOR to use in building Award Routing and Notification Rules

--- kcmit-db/kcmit-db-sql/src/main/resources/edu/mit/kc/sql/6.0/dml/DML_MITKC-973_B000_20150211.sql
+++ kcmit-db/kcmit-db-sql/src/main/resources/edu/mit/kc/sql/6.0/dml/DML_MITKC-973_B000_20150211.sql
@@ -2,7 +2,7 @@
 insert into KRMS_FUNC_T
 	(FUNC_ID,NM,DESC_TXT,RTRN_TYP,VER_NBR,ACTV,
 		TYP_ID,NMSPC_CD) 
-	values (CONCAT('KCMIT', KRMS_FUNC_S.NEXTVAL),'allAwards','Include all awards','java.lang.String',1,'Y',
+	values (CONCAT('KCMIT', KRMS_FUNC_S.NEXTVAL),'allAwards','Include all awards','java.lang.Boolean',1,'Y',
 		(select TYP_ID from KRMS_TYP_T where NMSPC_CD = 'KC-AWARD' and NM = 'Award Java Function Term Service'),'KC-AWARD')
 /
 insert into KRMS_FUNC_PARM_T (FUNC_PARM_ID,FUNC_ID,NM,DESC_TXT,TYP,SEQ_NO) 
@@ -11,7 +11,10 @@ insert into KRMS_FUNC_PARM_T (FUNC_PARM_ID,FUNC_ID,NM,DESC_TXT,TYP,SEQ_NO)
 /
 insert into KRMS_TERM_SPEC_T (TERM_SPEC_ID, NMSPC_CD, NM, DESC_TXT, TYP, ACTV, VER_NBR) 
 	values (CONCAT('KCMIT', KRMS_TERM_SPEC_S.NEXTVAL),'KC-AWARD',(select FUNC_ID from KRMS_FUNC_T where  NM='allAwards' and NMSPC_CD='KC-AWARD'),
-			'Include all awards','java.lang.String','Y',1)
+			'Include all awards','java.lang.Boolean','Y',1)
+/
+insert into KRMS_TERM_T (TERM_ID, TERM_SPEC_ID, VER_NBR, DESC_TXT) values (CONCAT('KCMIT', KRMS_TERM_S.NEXTVAL),(select TERM_SPEC_ID from KRMS_TERM_SPEC_T where NMSPC_CD='KC-AWARD' and  
+NM=(select FUNC_ID from KRMS_FUNC_T where  NM='allAwards' and NMSPC_CD='KC-AWARD')),1,'Include all awards')
 /
 insert into KRMS_CNTXT_VLD_TERM_SPEC_T (CNTXT_TERM_SPEC_PREREQ_ID, CNTXT_ID, TERM_SPEC_ID, PREREQ) 
 	values (CONCAT('KCMIT', KRMS_CNTXT_VLD_TERM_SPEC_S.NEXTVAL),'KC-AWARD-CONTEXT',(select TERM_SPEC_ID from KRMS_TERM_SPEC_T where NMSPC_CD='KC-AWARD' and 
