f69ee40ec3da91f65776a8f6c488879cc1243373
Geo Thomas <geot@mit.edu>
2015-02-16 14:12:42 -0500

    changed all KRAD kew files and AwardBudget kew file

--- kcmit-db/kcmit-db-sql/src/main/resources/edu/mit/kc/sql/kcstage/Inactivate_role_mbr_records.sql
+++ kcmit-db/kcmit-db-sql/src/main/resources/edu/mit/kc/sql/kcstage/Inactivate_role_mbr_records.sql
@@ -2,17 +2,25 @@ update krim_role_mbr_t set  actv_to_dt  = sysdate
 	--select count(*) from krim_role_mbr_t 
   where mbr_id not in (
 	select prncpl_id from  krim_prncpl_t 
-	where prncpl_nm in ('admin','geot','sdowdy','cwood','aeh','snair','kr')
-	)
-	and role_id not in (select role_id from krim_roleT where nm like '%Award Budget%');
+          where prncpl_nm in ('admin','geot','sdowdy','cwood','snair','kr','kc')
+        )
+	and 
+  --select count(*) from krim_role_mbr_t where 
+  role_id not in (select role_id from krim_role_t where role_nm like '%Award Budget%');
   
 update krim_role_mbr_t set  actv_to_dt  = sysdate   
   where mbr_id in (
 	select prncpl_id from  krim_prncpl_t 
-	where prncpl_nm ='aeh' and
-	role_id not in (select role_id from krim_role_t where role_nm='Application Administrator')
+	where --prncpl_nm ='aeh' and
+	role_id in (select role_id from krim_role_t where role_nm='Manager')
 	);  
   
+  
+update krim_role_mbr_t set  actv_to_dt  = null   
+  where mbr_id in (
+	select prncpl_id from  krim_prncpl_t 
+	where prncpl_nm ='aeh'
+	); 
   
 select * from krim_role_perm_t where role_id in (select role_id from krim_role_t where role_nm like '%Award Budget%')
 
@@ -23,3 +31,23 @@ select * from krim_role_t where role_nm='User'
 select count(*) from krim_role_mbr_t where mbr_id in (select prncpl_id from krim_prncpl_t where prncpl_id='admin')
 
 update krim_prncpl_t set ACTV_IND='Y' where prncpl_nm in ('admin','kr')
+
+
+select count(*) from krim_role_mbr_t 
+  where mbr_id in (
+	select prncpl_id from  krim_prncpl_t 
+	where prncpl_nm in ('kc')
+	)
+  
+  
+select * from krim_role_mbr_t 
+--update krim_role_mbr_t set  actv_to_dt  = null
+where 
+  role_id in (select role_id from krim_role_t where role_nm='Manager');  
+
+
+update krim_role_mbr_t set  actv_to_dt  = null
+where 
+  role_id in (select role_id from krim_role_t where role_nm='Kuali Rules Management System Administrator');  
+  
+  
