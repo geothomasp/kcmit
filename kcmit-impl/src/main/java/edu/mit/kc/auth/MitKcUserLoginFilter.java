0594733bf8640b51b1ae197ca36ce43884e0c312
Geo Thomas <geot@mit.edu>
2015-02-17 17:30:24 -0500

    Files required to merge KCC 6.0.0 sprint 15

--- kcmit-impl/src/main/java/edu/mit/kc/auth/MitKcUserLoginFilter.java
+++ kcmit-impl/src/main/java/edu/mit/kc/auth/MitKcUserLoginFilter.java
@@ -2,7 +2,7 @@
  * Copyright 2005-2008 The Kuali Foundation
  * 
  * 
- * Licensed under the Educational Community License, Version 2.0 (the "License");
+ * Licensed under the GNU Affero General Public License, Version 3 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  * 
