f7a2ee0414a5bc789f71a122028b3df45a1e00a8
rkprasad <rkprasad@polus215.polusindia.com>
2015-02-23 22:01:53 +0530

    MITKC-1117

--- kcmit-db/kcmit-db-sql/src/main/resources/edu/mit/kc/sql/6.0/dml/DML_MITKC-799_02162015.sql
+++ kcmit-db/kcmit-db-sql/src/main/resources/edu/mit/kc/sql/6.0/dml/DML_MITKC-799_02162015.sql
@@ -92,5 +92,26 @@ if li_count = 0 then
 	
 end if;
 
+select count(role_id) 
+into li_count 
+from KRIM_ROLE_T where ROLE_NM = 'View Subcontract Documents';
+if li_count = 0 then
+	INSERT INTO KRIM_ROLE_T(ROLE_ID,OBJ_ID,VER_NBR,ROLE_NM,NMSPC_CD,DESC_TXT,KIM_TYP_ID,ACTV_IND, LAST_UPDT_DT)  
+	VALUES(KRIM_ROLE_ID_S.NEXTVAL,SYS_GUID(),1,'View Subcontract Documents','KC-SUBAWARD','View Subcontract Documents','69','Y',sysdate);	
+end if;
+
+select count(role_perm_id)
+  into li_count
+    from KRIM_ROLE_PERM_T
+    where role_id = (select  ROLE_ID from KRIM_ROLE_T where trim(upper(ROLE_NM)) = trim(upper('View Subcontract Documents')))
+    and   PERM_ID = (select PERM_ID from KRIM_PERM_T where nm = 'VIEW_SUBAWARD_DOCUMENTS' );
+    
+    if li_count = 0 then
+      INSERT INTO KRIM_ROLE_PERM_T(ROLE_PERM_ID,OBJ_ID,VER_NBR,ROLE_ID,PERM_ID,ACTV_IND)
+      VALUES(KRIM_ROLE_PERM_ID_S.nextval,sys_guid(),1,
+      (select  ROLE_ID from KRIM_ROLE_T where trim(upper(ROLE_NM)) = trim(upper('View Subcontract Documents'))),
+      (select PERM_ID from KRIM_PERM_T where nm = 'VIEW_SUBAWARD_DOCUMENTS' ),
+      'Y');    
+    end if;
 end;
 /
