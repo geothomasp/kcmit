7124bf94ee31305ca960a51c1993a7560f4b9e10
Geo Thomas <geot@mit.edu>
2015-03-05 15:51:03 -0500

    more staging scripts

--- kcmit-db/kcmit-db-sql/src/main/resources/edu/mit/kc/sql/kcstage/Inactivate_role_mbr_records.sql
+++ kcmit-db/kcmit-db-sql/src/main/resources/edu/mit/kc/sql/kcstage/Inactivate_role_mbr_records.sql
@@ -51,3 +51,79 @@ where
   role_id in (select role_id from krim_role_t where role_nm='Kuali Rules Management System Administrator');  
   
   
+update krim_role_mbr_t set  actv_to_dt  = null 
+	--select count(*) from krim_role_mbr_t 
+  where mbr_id in (
+	select prncpl_id from  krim_prncpl_t 
+          where prncpl_nm in ('acia',
+                  'mbaguer',
+                  'wbarrett',
+                  'sarahb',
+                  'pcaissie',
+                  'icariolo',
+                  'mchristy',
+                  'mclarkso',
+                  'mcorcor',
+                  'kdawson',
+                  'kdenutte',
+                  'jpd',
+                  'ncaissie',
+                  'sdowdy',
+                  'rdownes',
+                  'tduff',
+                  'mff',
+                  'nfoley',
+                  'rfrost',
+                  'mgnetto',
+                  'jrgold',
+                  'cgoodwin',
+                  'rgrewal',
+                  'ehall',
+                  'rhanlon',
+                  'cheaton',
+                  'aeh',
+                  'laureena',
+                  'hudak',
+                  'i_annac',
+                  'rlateva',
+                  'rlay',
+                  'klazzaro',
+                  'mleskiw',
+                  'jenlu',
+                  'kmann',
+                  'rmathews',
+                  'mam',
+                  'tmelende',
+                  'vmorris',
+                  'snair',
+                  'lnelson',
+                  'cnewf',
+                  'dan4th',
+                  'kiirja',
+                  'spettit',
+                  'lpiazza',
+                  'hponnoju',
+                  'powderly',
+                  'dquimby',
+                  'jdrob',
+                  'jennymr',
+                  'nsahag',
+                  'ele',
+                  'kshikes',
+                  'sullaway',
+                  'geot',
+                  'bvallely',
+                  'pviejo',
+                  'svogel',
+                  'cwood'
+                )
+
+        )
+	and role_id in (select role_id from krim_role_t where role_nm = 'View Award');
+
+select count(*) from krim_role_mbr_t 
+--update krim_role_mbr_t set  actv_to_dt  = null 
+ where mbr_id in (
+	select prncpl_id from  krim_prncpl_t 
+          where prncpl_nm in ('acia') and role_id in (select role_id from krim_role_t where role_nm = 'View Award'))
+
