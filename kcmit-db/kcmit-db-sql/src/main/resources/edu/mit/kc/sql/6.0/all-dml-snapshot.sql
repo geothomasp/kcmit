95ac4e6e3b6428a2b1622b249ce654a2730c50dc
anumole <anumole@polus177.polusindia.com>
2015-03-13 12:58:08 +0530

    committed script for MITKC-824

--- kcmit-db/kcmit-db-sql/src/main/resources/edu/mit/kc/sql/6.0/all-dml-snapshot.sql
+++ kcmit-db/kcmit-db-sql/src/main/resources/edu/mit/kc/sql/6.0/all-dml-snapshot.sql
@@ -1,4 +1,5 @@
 @dml/DML_MITKC-1071_20150309.sql
 @dml/DML_MITKC-611_20150311.sql
+@dml/DML_MITKC-824_20150313.sql
 commit
 /
--- kcmit-db/kcmit-db-sql/src/main/resources/edu/mit/kc/sql/6.0/dml/DML_MITKC-611_02262015.sql
+++
@@ -1,2 +0,0 @@
-Insert into KRCR_PARM_T (NMSPC_CD,CMPNT_CD,PARM_NM,OBJ_ID,VER_NBR,PARM_TYP_CD,VAL,PARM_DESC_TXT,EVAL_OPRTR_CD,APPL_ID) values ('KC-GEN','Document','KC_COI_DB_LINK',sys_guid(),1,'AUTH','COEUS.KUALI','DB Link to connect with Coeus DB to get the COI Functions','A','KUALI')
-/
