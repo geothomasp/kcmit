115a585f5dd926848347b77481646cee1ad74714
rmancher <rajeev@genesistec.com>
2015-03-24 10:50:47 -0400

    MITKC-1233 Dashboard dmls

---
+++ kcmit-db/kcmit-db-sql/src/main/resources/edu/mit/kc/sql/6.0/dml/DML_MITKC-1233_03242015.sql
@@ -0,0 +1,33 @@
+INSERT INTO DASH_BOARD_MENU_ITEMS (DASH_BOARD_MENU_ITEM_ID, MENU_ITEM, MENU_ACTION, OSP_ONLY_FLAG, ACTIVE, UPDATE_USER)
+VALUES(SEQ_DASH_BOARD_MENU_ITEM_ID.NEXTVAL, 'Create Proposal', '/kc-pd-krad/proposalDevelopment?methodToCall=docHandler&command=initiate&viewId=PropDev-InitiateView', 'N', 'Y', 'admin')
+/
+
+INSERT INTO DASH_BOARD_MENU_ITEMS (DASH_BOARD_MENU_ITEM_ID, MENU_ITEM, MENU_ACTION, OSP_ONLY_FLAG, ACTIVE, UPDATE_USER)
+VALUES(SEQ_DASH_BOARD_MENU_ITEM_ID.NEXTVAL, 'Create Protocol', '/kc-krad/landingPage?methodToCall=start&HREF=<<APPLICATION_URL>>%2FprotocolProtocol.do%3FmethodToCall%3DdocHandler%26command%3Dinitiate%26docFormKey%3D88888888%26docTypeName%3DProtocolDocument%26returnLocation%3D<<APPLICATION_URL>>%252Fkc-krad%252FlandingPage%253FviewId%253DKc-LandingPage-RedirectView&viewId=Kc-Header-IframeView', 'N', 'Y', 'admin')
+/
+
+
+INSERT INTO DASH_BOARD_MENU_ITEMS (DASH_BOARD_MENU_ITEM_ID, MENU_ITEM, MENU_ACTION, OSP_ONLY_FLAG, ACTIVE, UPDATE_USER)
+VALUES(SEQ_DASH_BOARD_MENU_ITEM_ID.NEXTVAL, 'Create Protocol', '/kc-krad/landingPage?methodToCall=start&HREF=<<APPLICATION_URL>>%2FprotocolProtocol.do%3FmethodToCall%3DdocHandler%26command%3Dinitiate%26docFormKey%3D88888888%26docTypeName%3DProtocolDocument%26returnLocation%3D<<APPLICATION_URL>>%252Fkc-krad%252FlandingPage%253FviewId%253DKc-LandingPage-RedirectView&viewId=Kc-Header-IframeView', 'N', 'Y', 'admin')
+/
+
+INSERT INTO DASH_BOARD_MENU_ITEMS (DASH_BOARD_MENU_ITEM_ID, MENU_ITEM, MENU_ACTION, OSP_ONLY_FLAG, ACTIVE, UPDATE_USER)
+VALUES(SEQ_DASH_BOARD_MENU_ITEM_ID.NEXTVAL, 'Create Protocol', '/kc-krad/landingPage?methodToCall=start&HREF=<<APPLICATION_URL>>%2FprotocolProtocol.do%3FmethodToCall%3DdocHandler%26command%3Dinitiate%26docFormKey%3D88888888%26docTypeName%3DProtocolDocument%26returnLocation%3D<<APPLICATION_URL>>%252Fkc-krad%252FlandingPage%253FviewId%253DKc-LandingPage-RedirectView&viewId=Kc-Header-IframeView', 'N', 'Y', 'admin')
+/
+
+INSERT INTO DASH_BOARD_MENU_ITEMS (DASH_BOARD_MENU_ITEM_ID, MENU_ITEM, MENU_ACTION, OSP_ONLY_FLAG, ACTIVE, UPDATE_USER)
+VALUES(SEQ_DASH_BOARD_MENU_ITEM_ID.NEXTVAL, 'Search for Proposals', '/kr-krad/lookup?methodToCall=start&viewId=DevelopmentProposals-LookupViewId', 'N', 'Y', 'admin')
+/
+
+INSERT INTO DASH_BOARD_MENU_ITEMS (DASH_BOARD_MENU_ITEM_ID, MENU_ITEM, MENU_ACTION, OSP_ONLY_FLAG, ACTIVE, UPDATE_USER)
+VALUES(SEQ_DASH_BOARD_MENU_ITEM_ID.NEXTVAL, 'View Proposals Enroute', '/kr-krad/lookup?methodToCall=start&viewId=EnrouteDevelopmentProposals-LookupViewId', 'N', 'Y', 'admin')
+/
+
+INSERT INTO DASH_BOARD_MENU_ITEMS (DASH_BOARD_MENU_ITEM_ID, MENU_ITEM, MENU_ACTION, OSP_ONLY_FLAG, ACTIVE, UPDATE_USER)
+VALUES(SEQ_DASH_BOARD_MENU_ITEM_ID.NEXTVAL, 'View All My Proposals', '/kr-krad/lookup?methodToCall=search&viewId=AllDevelopmentProposals-LookupViewId', 'N', 'Y', 'admin')
+/
+
+INSERT INTO DASH_BOARD_MENU_ITEMS (DASH_BOARD_MENU_ITEM_ID, MENU_ITEM, MENU_ACTION, OSP_ONLY_FLAG, ACTIVE, UPDATE_USER)
+VALUES(SEQ_DASH_BOARD_MENU_ITEM_ID.NEXTVAL, 'Create Institute Proposal', '/kc-krad/landingPage?methodToCall=start&href=<<APPLICATION_URL>>%2Fkr%2Flookup.do%3FmethodToCall%3Dstart%26businessObjectClassName%3Dorg.kuali.kra.institutionalproposal.proposallog.ProposalLog%26docFormKey%3D88888888%26includeCustomActionUrls%3Dtrue%26returnLocation%3D<<APPLICATION_URL>>%252Fkc-krad%252FlandingPage%253FviewId%253DKc-LandingPage-RedirectView%3FforInstitutionalProposal%26hideReturnLink%3Dtrue%26showMaintenanceLinks%3Dtrue%26refreshCaller%3Dcancel&viewId=Kc-Header-IframeView', 'N', 'Y', 'admin')
+/
+
