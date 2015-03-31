8a55a4c3827087e58a497c56e3dcdd490634fc10
Geo Thomas <geot@mit.edu>
2015-03-30 21:13:34 -0400

    reset all kcc dml snapshots

--- coeus-impl/src/main/java/org/kuali/kra/institutionalproposal/service/impl/InstitutionalProposalServiceImpl.java
+++ coeus-impl/src/main/java/org/kuali/kra/institutionalproposal/service/impl/InstitutionalProposalServiceImpl.java
@@ -488,11 +488,9 @@ public class InstitutionalProposalServiceImpl implements InstitutionalProposalSe
         if (developmentProposal.getRolodex() != null) {
             institutionalProposal.setRolodexId(developmentProposal.getRolodex().getRolodexId());
         }
-   /*     if(institutionalProposal.getAwardType()!=null && developmentProposal.getAnticipatedAwardType()!=null){
-        institutionalProposal.getAwardType().setDescription(developmentProposal.getAnticipatedAwardType().getDescription());
-        institutionalProposal.setAwardTypeCode(developmentProposal.getAnticipatedAwardType().getCode());
-        }    */
-        institutionalProposal.setAwardTypeCode(developmentProposal.getAnticipatedAwardType().getCode());
+        if(developmentProposal.getAnticipatedAwardType()!=null){
+        	institutionalProposal.setAwardTypeCode(developmentProposal.getAnticipatedAwardType().getCode());
+        }
     }
     
     protected void doCustomAttributeDataFeed(InstitutionalProposalDocument institutionalProposalDocument, DevelopmentProposal developmentProposal) throws WorkflowException {
