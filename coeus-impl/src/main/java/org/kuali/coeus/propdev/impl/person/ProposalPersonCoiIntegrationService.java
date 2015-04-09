abf1af04b6a6ecb8cd72dcaaf74d1cc9e870931b
vineeth <vineeth@mit.edu>
2015-04-09 19:34:57 -0400

    MITKC-611-COI integration

---
+++ coeus-impl/src/main/java/org/kuali/coeus/propdev/impl/person/ProposalPersonCoiIntegrationService.java
@@ -0,0 +1,10 @@
+package org.kuali.coeus.propdev.impl.person;
+
+public interface ProposalPersonCoiIntegrationService {
+	
+	/*
+	 * This method will check the questions are completed and any of the COI questions answered “yes”.
+	 */
+	public boolean isCoiQuestionsAnswered(ProposalPerson proposalPerson);
+
+}
