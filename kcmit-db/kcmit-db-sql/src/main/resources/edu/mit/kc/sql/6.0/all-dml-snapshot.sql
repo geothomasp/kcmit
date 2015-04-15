b2d66499336f279df0d37636135aaf9c667a4957
bsmith83 <kubsmith@ucdavis.edu>
2015-04-15 01:21:03 -0700

    [MITKC-1093] Sap feed maintenance part 1

--- kcmit-db/kcmit-db-sql/src/main/resources/edu/mit/kc/sql/6.0/all-dml-snapshot.sql
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-@dml/DML_MITKC-1107_20150315.sql
-=======
-@dml/DML_MITKC-1403_04142015.sql
-@dml/add_new_person_mitkc.sql
-@dml/DML_MITKC-1107_20150314.sql
->>>>>>> MITKC-1107
-commit
-/
