58e62ff352ab53c482296b7c803d6196720a0dbd
Geo Thomas <geot@mit.edu>
2015-04-21 17:12:23 -0400

    reset all snapshots

--- kcmit-db/kcmit-db-sql/src/main/resources/edu/mit/kc/sql/6.0/all-dml-snapshot.sql
+++
@@ -1,4 +0,0 @@
-@dml/dml_wl_flexibility.sql
-@dml/kco-merge-602.sql
-commit
-/
--- kcmit-db/kcmit-db-sql/src/main/resources/edu/mit/kc/sql/migration/all-migration-snapshot.sql
+++
@@ -1,6 +0,0 @@
-@../feed/sap/kc_sap_feed_pkg.sql
-@DML_MITKC-1146_04212015.sql
-@DML_MITKC-1189_04212015.sql
-commit;
-exit
-/
