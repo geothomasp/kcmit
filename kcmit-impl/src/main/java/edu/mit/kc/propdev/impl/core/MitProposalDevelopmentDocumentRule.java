6084e2b1dd18346a2cefb29ecf7efaf722911442
rmancher <rajeev@genesistec.com>
2015-05-05 22:46:50 -0400

    Revert temp migration fix
    This reverts commit 7c8ec9eefb645ad293f3b488dd61ca1e6be8ad6e.
    
    Conflicts:
    	kcmit-impl/src/main/java/org/kuali/coeus/propdev/impl/basic/ProposalDevelopmentHomeController.java

--- kcmit-impl/src/main/java/edu/mit/kc/propdev/impl/core/MitProposalDevelopmentDocumentRule.java
+++ kcmit-impl/src/main/java/edu/mit/kc/propdev/impl/core/MitProposalDevelopmentDocumentRule.java
@@ -14,22 +14,22 @@ public class MitProposalDevelopmentDocumentRule extends ProposalDevelopmentDocum
 	  @Override
 	    public boolean processSaveDocument(Document document) {
 	        boolean isValid = true;
-//	        ProposalDevelopmentDocument proposalDevelopmentDocument = (ProposalDevelopmentDocument)document;
-//	        if(proposalDevelopmentDocument.getDevelopmentProposal()!=null && (proposalDevelopmentDocument.getDevelopmentProposal().getProposalStateTypeCode().equals(ProposalState.IN_PROGRESS) ||
-//	        		proposalDevelopmentDocument.getDevelopmentProposal().getProposalStateTypeCode().equals(ProposalState.REVISIONS_REQUESTED))){
-//	        	isValid = isDocumentOverviewValid(document);
-//
-//	        	GlobalVariables.getMessageMap().addToErrorPath(KRADConstants.DOCUMENT_PROPERTY_NAME);
-//
-//	        	getKnsDictionaryValidationService().validateDocumentAndUpdatableReferencesRecursively(document, getMaxDictionaryValidationDepth(),
-//	        			VALIDATION_REQUIRED, CHOMP_LAST_LETTER_S_FROM_COLLECTION_NAME);
-//	        	getDictionaryValidationService().validateDefaultExistenceChecksForTransDoc((TransactionalDocument) document);
-//
-//	        	GlobalVariables.getMessageMap().removeFromErrorPath(KRADConstants.DOCUMENT_PROPERTY_NAME);
-//
-//	        	isValid &= GlobalVariables.getMessageMap().hasNoErrors();
-//	        	isValid &= processCustomSaveDocumentBusinessRules(document);
-//	        }
+	        ProposalDevelopmentDocument proposalDevelopmentDocument = (ProposalDevelopmentDocument)document;
+	        if(proposalDevelopmentDocument.getDevelopmentProposal()!=null && (proposalDevelopmentDocument.getDevelopmentProposal().getProposalStateTypeCode().equals(ProposalState.IN_PROGRESS) ||
+	        		proposalDevelopmentDocument.getDevelopmentProposal().getProposalStateTypeCode().equals(ProposalState.REVISIONS_REQUESTED))){
+	        	isValid = isDocumentOverviewValid(document);
+
+	        	GlobalVariables.getMessageMap().addToErrorPath(KRADConstants.DOCUMENT_PROPERTY_NAME);
+
+	        	getKnsDictionaryValidationService().validateDocumentAndUpdatableReferencesRecursively(document, getMaxDictionaryValidationDepth(),
+	        			VALIDATION_REQUIRED, CHOMP_LAST_LETTER_S_FROM_COLLECTION_NAME);
+	        	getDictionaryValidationService().validateDefaultExistenceChecksForTransDoc((TransactionalDocument) document);
+
+	        	GlobalVariables.getMessageMap().removeFromErrorPath(KRADConstants.DOCUMENT_PROPERTY_NAME);
+
+	        	isValid &= GlobalVariables.getMessageMap().hasNoErrors();
+	        	isValid &= processCustomSaveDocumentBusinessRules(document);
+	        }
 	        return isValid;
 	    }
 
