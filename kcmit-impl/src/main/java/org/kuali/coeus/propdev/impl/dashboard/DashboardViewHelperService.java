82997e37e513970f1ae53e328f332ae7493f4765
vineeth <vineeth@OSP414.mit.edu>
2015-05-29 21:52:41 -0400

    dashboard graph permission fix

--- kcmit-impl/src/main/java/org/kuali/coeus/propdev/impl/dashboard/DashboardViewHelperService.java
+++ kcmit-impl/src/main/java/org/kuali/coeus/propdev/impl/dashboard/DashboardViewHelperService.java
@@ -1,5 +1,6 @@
 package org.kuali.coeus.propdev.impl.dashboard;
 
+import org.kuali.coeus.sys.framework.gv.GlobalVariableService;
 import org.kuali.kra.award.home.Award;
 import org.kuali.rice.core.api.CoreApiServiceLocator;
 import org.kuali.rice.kew.api.KewApiConstants;
@@ -27,6 +28,11 @@ import java.util.Properties;
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class DashboardViewHelperService extends ViewHelperServiceImpl {
 
+	 @Autowired
+	 @Qualifier("globalVariableService")
+	 private GlobalVariableService globalVariableService;
+
+	
 
 	public static String buildPropDevLink(String docId) throws WorkflowException {
         return CoreApiServiceLocator.getKualiConfigurationService().getPropertyValueAsString(KRADConstants.WORKFLOW_URL_KEY)
@@ -77,5 +83,20 @@ public class DashboardViewHelperService extends ViewHelperServiceImpl {
     protected static String getHtmlActionKeyPerson() {
         return "keyperson.do";
     }
+    
+    public boolean showDashBoardGraph(String dashBoardPerson) {
+    	boolean showDashBoardGraph = false;
+    	String loggedInPerson = getGlobalVariableService().getUserSession().getPrincipalId();
+    	if(loggedInPerson!=null && dashBoardPerson!=null && loggedInPerson.equals(dashBoardPerson)){
+    		showDashBoardGraph = true;
+    	}
+        return showDashBoardGraph;
+    }
+    public GlobalVariableService getGlobalVariableService() {
+		return globalVariableService;
+	}
 
+	public void setGlobalVariableService(GlobalVariableService globalVariableService) {
+		this.globalVariableService = globalVariableService;
+	}
 }
