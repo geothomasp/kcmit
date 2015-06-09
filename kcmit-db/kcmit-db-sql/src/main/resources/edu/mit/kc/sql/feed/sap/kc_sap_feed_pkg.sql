bb74fa4a03c5b2f3c7a674fb7f0f28a231cb5a8b
rmancher <rajeev@genesistec.com>
2015-06-09 16:50:24 -0400

    MITKC-1864 - update to select active award

--- kcmit-db/kcmit-db-sql/src/main/resources/edu/mit/kc/sql/feed/sap/kc_sap_feed_pkg.sql
+++ kcmit-db/kcmit-db-sql/src/main/resources/edu/mit/kc/sql/feed/sap/kc_sap_feed_pkg.sql
@@ -883,10 +883,10 @@ IF gs_parent_award_number <> '000000-00000' THEN
 	SELECT AWARD.ACCOUNT_NUMBER
     INTO gs_parent_account_number
     FROM AWARD
-   WHERE ( AWARD.AWARD_NUMBER = gs_parent_award_number ) AND
+   WHERE ( AWARD.AWARD_NUMBER = gs_parent_award_number AND AWARD.AWARD_SEQUENCE_STATUS = 'ACTIVE') AND
          ( AWARD.SEQUENCE_NUMBER = (SELECT MAX(AWARD2.SEQUENCE_NUMBER)
 													FROM AWARD AWARD2		 WHERE
-													AWARD2.AWARD_NUMBER =AWARD.AWARD_NUMBER AND AWARD_SEQUENCE_STATUS = 'ACTIVE'));
+													AWARD2.AWARD_NUMBER =AWARD.AWARD_NUMBER AND AWARD2.AWARD_SEQUENCE_STATUS = AWARD.AWARD_SEQUENCE_STATUS));
 ELSE
 
 /**********************************************************************************************
